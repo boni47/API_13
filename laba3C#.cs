c571e6db5740bce1343e11ce8a936f5d780654cd
boni <elenaber089@gmail.com>
2024-12-09 16:34:41 +0300

    Add files via upload

---
+++ laba3C#.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var usersResponse = await client.GetStringAsync("https://jsonplaceholder.typicode.com/users");
+            Console.WriteLine("Users:\n" + usersResponse);
+
+            int postId = 20 + 20;
+            var postResponse = await client.GetStringAsync($"https://jsonplaceholder.typicode.com/posts/{postId}");
+            Console.WriteLine($"\nPost {postId}:\n" + postResponse);
+
+            var commentsResponse = await client.GetStringAsync("https://jsonplaceholder.typicode.com/comments");
+            Console.WriteLine("\nComments:\n" + commentsResponse);
+
+            int commentId = 100 + 20;
+            var commentResponse = await client.GetStringAsync($"https://jsonplaceholder.typicode.com/comments/{commentId}");
+            Console.WriteLine($"\nComment {commentId}:\n" + commentResponse);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
